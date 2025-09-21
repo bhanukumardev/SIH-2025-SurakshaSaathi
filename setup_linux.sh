cdbd44646c570bab1ecbecd9651f2f5b53e2b7cd
kanishka <kanishka161203@gmail.com>
2025-09-22 05:26:37 +0530

    Added Installation files

---
+++ setup_linux.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Stop on errors
+set -e
+
+echo "Running npm install..."
+npm install 
+
+echo "Setting up Linux environment..."
+echo "Installing Package Manager (Astral UV)..."
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Enter the chatbot folder
+cd chatbot
+
+echo "Running 'uv sync'..."
+uv sync
+
+echo "Downloading NLTK data by running the Python script..."
+./.venv/bin/python3 script.py
+
+echo "Python virtual environment setup complete."
+echo "Activate the virtual environment using the command:"
+echo "source ./.venv/bin/activate"
+
+# Return to the previous directory
+cd ..
+
